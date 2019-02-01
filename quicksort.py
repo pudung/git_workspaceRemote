fb420f7b397934334d551e81e2810ebf2aab97e7
Yeseul Kang <yesel6089@gmail.com>
2019-02-01 13:55:52 +0900

    added input code

---
+++ quicksort.py
@@ -0,0 +1,17 @@
+def quick_sort(list):
+    arr_length = len(list)
+    if arr_length <=1:
+        return list
+    else:
+        pivot = list[0]
+        lesser = [element for element in list[1:] if element < pivot]
+        greater = [element for element in list[1:] if element > pivot]
+        return quick_sort(lesser) + [pivot] + quick_sort(greater)
+
+# 리스트를 입력받아서 quick_sort
+print("원소의 갯수를 입력해주세요 1<= n <= 1000\n")
+list = []
+size = input()
+for i in range(0,int(size)):
+    list.append(int(input()))
+print(quick_sort(list))
